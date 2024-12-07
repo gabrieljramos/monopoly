d89fac733c7713ad627558e4c3ae7c91fb1bc218
Gabriel Justus Ramos <gabrielramos1@ufpr.br>
2024-12-07 09:42:56 -0300

    Update run.java

--- src/run.java
+++ src/run.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 public class run extends Application {
 
     private boolean quit = false;
+    private String lastKeyPressed = "";
 
     public static void main(String[] args) {
         launch(args); // Inicializa o JavaFX
@@ -52,18 +53,44 @@ public class run extends Application {
         System.out.println("Inicializando o jogo...");
     }
 
+    private void detectEnterOrEsc(Scene scene) {
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                lastKeyPressed = "ENTER";
+            } else if (event.getCode() == KeyCode.ESCAPE) {
+                lastKeyPressed = "ESC";
+            }
+        });
+    }
+
     private void gameLoop(int totalPlayers) {
         // Implementar lógica do loop do jogo
+        int currentPlayer = 0;
         double FPS = 60;
         double drawInterval = 1000000000 / FPS;
         double nextDrawTime = System.nanoTime() + drawInterval;
 
         while (!quit) {
-            for (int currentPlayer = 0; currentPlayer < totalPlayers; currentPlayer++) {
-                System.out.println("Jogador " + (currentPlayer + 1) + " está jogando.");
-                // Implementar a lógica do jogador
+            // Desenha o tabuleiro
+            System.out.println("Jogador " + (currentPlayer + 1) + " está jogando.");
+            detectEnterOrEsc(scene);
+            if (lastKeyPressed.equals("ENTER")) {
+                board.gamers[currentPlayer].move(board.map.properties.get(board.gamers[currentPlayer].position),board.dado,board.map.properties.size());
+                board.gamers[currentPlayer].bankruptcy = board.gamers[currentPlayer].update();
+                if (board.gamers[currentPlayer].verifyOwnership == false) {
+                    //Chama metodos pedindo se ele quer comprar a propriedade do banco ou outro player
+                }
+                else {
+                    board.gamers[currentPlayer].improveProperty();
+                }
             }
-
+            else if (lastKeyPressed.equals("ESQ")) {
+                pauseMenu();
+            }
+            board.gamers[currentPlayer].checkVictory();
+            currentPlayer++;
+            if (currentPlayer >= totalPlayers)
+                currentPlayer = 0;
             // Temporizador para FPS
             try {
                 double remainingTime = nextDrawTime - System.nanoTime();
@@ -78,4 +105,3 @@ public class run extends Application {
         }
     }
 }
-

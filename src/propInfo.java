963fcb2cddb19cdf3238fe155cf249c9d72aa330
Gabriel Justus Ramos <gabrielramos1@ufpr.br>
2024-12-06 21:22:34 -0300

    Update propInfo.java

--- src/propInfo.java
+++ src/propInfo.java
@@ -5,7 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.lang.Math;
 
-class propInfo {
+public class propInfo {
 
     int numLines;
     List<String> allLines = new ArrayList<String>();

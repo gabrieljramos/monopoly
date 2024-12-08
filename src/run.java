4276426e6d0091dbdb47b4b0488d5946f02ce5fa
Gabriel Justus Ramos <gabrielramos1@ufpr.br>
2024-12-08 11:27:58 -0300

    Update run.java

--- src/run.java
+++ src/run.java
@@ -43,6 +43,7 @@ public class run extends Application {
             prop.value[0] = 1000;
             prop.houses[0] = 1500;
             prop.rent[0] = 500;
+            tabuleiro.map[0] = prop;
             startGameLoop(playerAmount, tabuleiro, scene, primaryStage);
         });
 

2ca4c1a11845bb73b656ab185fe6f8ba626f10d2
Gabriel Justus Ramos <gabrielramos1@ufpr.br>
2024-12-08 16:03:08 -0300

    Update propInfo.java

--- src/propInfo.java
+++ src/propInfo.java
@@ -53,11 +53,11 @@ public class propInfo {
         return Integer.parseInt(parts[3]);
         
     }
-    public int getMultiplier(int pos) {
+    public int getMultiplier(int pos, int which) {
         
         String propInfo = getPropertyInfo(pos);
         String[] parts = propInfo.split("[,]");
-        return Integer.parseInt(parts[4]);
+        return Float.parseFloat(parts[4 + which]);
         
     }
     public int getSetType(int pos) {
